32219f0d6bcbb6039627dc16a6b8cc9eafe3eb7c
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-31 11:59:20 -0500

    refactor

--- index.js
+++ index.js
@@ -7,14 +7,27 @@
 
 'use strict';
 
-var isArguments = require('is-arguments');
-var flatten = require('arr-flatten');
+var typeOf = require('kind-of');
 
 module.exports = function toPath(args) {
-  if (isArguments(args)) {
-    args = [].slice.call(args);
-  } else {
-    args = [].slice.call(arguments);
+  if (typeOf(args) !== 'arguments') {
+    args = arguments;
   }
-  return flatten(args).join('.');
+  return filter(args).join('.');
 };
+
+function filter(arr) {
+  var len = arr.length;
+  var idx = -1;
+  var res = [];
+
+  while (++idx < len) {
+    var ele = arr[idx];
+    if (typeOf(ele) === 'arguments' || Array.isArray(ele)) {
+      res.push.apply(res, filter(ele));
+    } else if (typeof ele === 'string') {
+      res.push(ele);
+    }
+  }
+  return res;
+}
--- test.js
+++ test.js
@@ -4,30 +4,51 @@ require('mocha');
 var assert = require('assert');
 var toPath = require('./');
 
-describe('toPath', function () {
-  it('should create an object path from a list of strings:', function () {
+describe('toPath', function() {
+  it('should create an object path from a list of strings:', function() {
     assert.equal(toPath('foo', 'bar', 'baz'), 'foo.bar.baz');
   });
 
-  it('should create an object path from an array of strings:', function () {
+  it('should create an object path from an array of strings:', function() {
     assert.equal(toPath(['foo', 'bar', 'baz']), 'foo.bar.baz');
   });
 
-  it('should create an object path from a list of arguments:', function () {
+  it('should flatten array args', function() {
+    assert.equal(toPath(['foo', ['bar', ['baz']]]), 'foo.bar.baz');
+  });
+
+  it('should create an object path from a list of arguments:', function() {
     function foo() {
       return toPath(arguments);
     }
     assert.equal(foo('foo', 'bar', 'baz'), 'foo.bar.baz');
   });
 
-  it('should create an object path from an array of arguments:', function () {
+  it('should create an object path from an array of arguments:', function() {
     function foo() {
       return toPath(arguments);
     }
     assert.equal(foo(['foo', 'bar', 'baz']), 'foo.bar.baz');
   });
 
-  it('should create an object path from a mixture of arrays and strings:', function () {
+  it('should work when arguments is not the first value:', function() {
+    function foo() {
+      return toPath('options', arguments);
+    }
+    assert.equal(foo(['foo', 'bar', 'baz']), 'options.foo.bar.baz');
+  });
+
+  it('should not use non-string values in the path', function() {
+    function foo() {
+      return toPath('options', arguments);
+    }
+    var fn = function() {
+      return;
+    };
+    assert.equal(foo([fn, 'foo', 'bar', 'baz'], fn), 'options.foo.bar.baz');
+  });
+
+  it('should create an object path from a mixture of arrays and strings:', function() {
     assert.equal(toPath('foo', ['bar', 'baz']), 'foo.bar.baz');
     assert.equal(toPath(['foo'], ['bar'], ['baz']), 'foo.bar.baz');
     assert.equal(toPath(['foo'], ['bar'], ['baz']), 'foo.bar.baz');
